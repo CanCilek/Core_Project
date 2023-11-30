842e90f3f47fb9adc25a3adaaa1bcbf08fe2ca02
Can Çilek <46296419+CanCilek@users.noreply.github.com>
2023-12-01 01:32:09 +0300

    Add Project Page in Admin Panel

---
+++ Core_Project/Controllers/PortfolioController.cs
@@ -0,0 +1,39 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Project.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        PortfolioManager portfolioManager = new PortfolioManager(new EfPortfolioDal());
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Proje Listesi";
+            ViewBag.v2 = "Proje";
+            ViewBag.v3 = "Proje Listesi";
+            var values = portfolioManager.TGetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddProject()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddProject(Portfolio p)
+        {
+            ViewBag.v1 = "Proje Ekleme";
+            ViewBag.v2 = "Proje";
+            ViewBag.v3 = "Proje Ekleme";
+            portfolioManager.TAdd(p);
+            return RedirectToAction("Index");
+        }
+    }
+}
