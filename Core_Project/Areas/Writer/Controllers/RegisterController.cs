70c00399846665a2a70a92633d62a9965f90d445
Can Çilek <46296419+CanCilek@users.noreply.github.com>
2024-01-12 15:03:36 +0300

    Login Page

---
+++ Core_Project/Areas/Writer/Controllers/RegisterController.cs
@@ -0,0 +1,63 @@
+﻿using Core_Project.Areas.Writer.Models;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Project.Areas.Writer.Controllers
+{
+    [Area("Writer")]
+    public class RegisterController : Controller
+    {
+
+        private readonly UserManager<WriterUser> _userManager;
+
+        public RegisterController(UserManager<WriterUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new UserRegisterViewModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(UserRegisterViewModel p)
+        {
+
+                WriterUser w = new WriterUser()
+                {
+                    Name = p.Name,
+                    Surname = p.Surname,
+                    UserName = p.UserName,
+                    Email = p.Mail,
+                    ImageUrl = p.ImageUrl
+
+                };
+            if (p.Password == p.ConfirmPassword)
+            {
+                var result = await _userManager.CreateAsync(w, p.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Login");
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+            }
+            return View(p);
+        }
+    }
+}
+//caN648698* CanCilek
+//123456789Aa* Jedi
